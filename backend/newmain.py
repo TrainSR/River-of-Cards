760f878b8fd7d7374dec4ff64e38e9fa329f9fbb
TrainSR <kha70929@gmail.com>
2025-11-21 17:02:38 +0700

    heak

--- backend/newmain.py
+++ backend/newmain.py
@@ -1,37 +1,11 @@
-#backend
-from threading import settrace
+schemaVersion: 1.2
 
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-import os
-from core.config import settings
-
-app = FastAPI(
-    title="Backend",
-    version="1.0",
-)
-
-cardpacks = ["ABC", "HEllp"]
-# Cho phép React frontend truy cập backend
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[settings.ALLOWED_ORIGIN],  # Vite dev server
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-@app.get("/get-string")
-def get_string():
-    return settings.DECK_FILE_ID
-
-# cd backend
-# uvicorn main:app --reload
-# version 3.10
-
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=5173, reload=True)  # cho phép truy cập từ mọi nơi
-
-
+endpoints:
+  - name: backend-api
+    displayName: Backend API
+    service:
+      basePath: /
+      port: 5173
+    type: REST
+    networkVisibilities:
+      - Public
