15c183d16237050c2aadc776ba1bdc19fab7814f
TrainSR <kha70929@gmail.com>
2025-11-21 09:40:54 +0700

    heak

--- src/mai.py
+++ src/mai.py
@@ -2,13 +2,16 @@
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import drive_module.drive_ops as drive_ops
 import os
 
 FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:5173")
 
 app = FastAPI()
 
-cardpacks = ["ABC", "HEllp"]
+cardpacks_id = "1u55lbW95eXte44VQLOrNFEXCkdWYTGBr"
+file_content = drive_ops.get_file_content(cardpacks_id)
+cardpacks = drive_ops.extract_yaml(file_content)["System_CardPacks"]
 # Cho phép React frontend truy cập backend
 app.add_middleware(
     CORSMiddleware,
